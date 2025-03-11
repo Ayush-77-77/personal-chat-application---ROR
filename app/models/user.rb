ba4641efa252a43466087150b0859166ec49b58e
Ayush-77-77 <ayush95758@gmail.com>
2025-03-11 15:42:54 +0530

    add the active storage for profile picture/avatar of users, user can upload their avatar at the time of sign up otherwise a default profile pic will be shown

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   has_many :contacts
   has_many :sent_messages, class_name: "Conversation", foreign_key: "sender_id"
   has_many :received_messages, class_name: "Conversation", foreign_key: "receiver_id"
+
+  has_one_attached :avatar
 end
