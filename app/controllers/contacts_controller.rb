94b8ad12b9e79244fc4402783dc7a427e72e684d
Ayush-77-77 <ayush95758@gmail.com>
2025-03-12 13:22:27 +0530

    add the create action to save the contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,20 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+  def create
+    contact = Contact.new(contact_params)
+    contact.user = current_user
+    if contact.save
+      flash[:notice] = "Contact saved"
+      redirect_to contacts_path(current_user)
+    else
+      flash[:alert] = "something went wrong"
+      render :new
+    end
+  end
+
+  private
+  def contact_params
+    params.expect(contact: [ :name, :phone_number ])
+  end
 end
